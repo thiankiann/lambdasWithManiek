232785ef5287ee2e29d36c2cceaa33311ddd11d9
@thiankiann <thiankiann@gmail.com>
2021-08-22 20:12:37 +0100

    [add] - Hello Lambda

---
+++ src/main/java/lambda/ManiekRunner.java
@@ -0,0 +1,12 @@
+package lambda;
+
+public class ManiekRunner implements Runner{
+
+    @Override
+    public void go() {
+        System.out.println("Run Maniek, run!");
+    }
+    public void walk() {
+        System.out.println("Walk Maniek");
+    }
+}
