9e8451a1a1a8508670a1bcfa0c50cf128dc34907
@thiankiann <thiankiann@gmail.com>
2021-09-11 16:39:33 +0100

    [change] - Optional + Generics

--- readme.md
+++ readme.md
@@ -3,6 +3,14 @@ Git - good practices
 * commit powinien miec dobry opis
 * commit powinien zawierac jedna zmiane (commit atomowy)
 * amend - zmiana opisu tylko dla ostatniego commit-a (doklejanie zmian do ostatniego commit bez tworzenia nowego)
+* podrozuj po historii i brunchach tylko przy czystym drzewie roboczym
+* nie usuwaj bruncha na ktorym aktualnie sie znajdujesz
+  
+* git flow - w skrocie
+    * master - brunch release- owy
+    * develop - bruch developerski
+    * feature branch - brunch rowijajacy dana funkcjonalnosc
+        * po winien startowac od developa i dno niego finalnie byc mergowanym
 
 
 Git - przydatne komendy
@@ -12,4 +20,5 @@ Git - przydatne komendy
 * git restore --staged file-name // zdejmij ze stage'a
 * git checkout branch-name // przełącz się na branch-name
 * git checkout -b branch-name // przełącz na nowo utworzony branch
-* git reflog 
+* git reflog - Reflog is a mechanism to record when the tip of branches are updated. This command is to manage the information recorded in it. Basically every action you perform inside of Git where data is stored, you can find it inside of the reflog
+* git log --graph  //rysujegraf historii w konsoli 
--- src/main/java/optional/MySecret.java
+++ src/main/java/optional/MySecret.java
@@ -1,19 +1,21 @@
 package optional;
 
+import java.util.Optional;
+
 import static java.util.Objects.nonNull;
 
-public class MySecret {
-    String mySecret ;
+public class MySecret<T> {
+    T mySecret ;
 
-    public MySecret(String mySecret) {
+    public MySecret(T mySecret) {
         this.mySecret = mySecret;
     }
 
-    public String getMySecret() {
+    public T getMySecret() {
         return mySecret;
     }
 
-    public void setMySecret(String mySecret) {
+    public void setMySecret(T mySecret) {
         this.mySecret = mySecret;
     }
 
@@ -25,12 +27,24 @@ public class MySecret {
     }
 
     public static void main(String[] args) {
-        MySecret realOne = new MySecret("I like learning and teaching Java");
-        MySecret withNull = new MySecret(null);
+        MySecret<String> realOne = new MySecret<>("I like learning and teaching Java");
+        MySecret<String> withNull = new MySecret<>(null);
         printSecretLength(realOne);
         printSecretLength(withNull);
+
+        MySecret<Integer> secretNumber = new MySecret<>(38);
+        MySecret<Integer> secretNullNumber = new MySecret<>(null);
+       // var secretNumOnr =secretNumber.getMySecret();  //42
+       // var secretNull = secretNullNumber.getMySecret(); // null
+
+        //zamiana na Optional<Integer>
+        Optional<Integer> secretNumOnr = Optional.ofNullable(secretNumber.getMySecret());  //38
+        Optional<Integer> secretNull = Optional.ofNullable(secretNullNumber.getMySecret()); // null
+
+
+
     }
-    public static void printSecretLength(MySecret mySecret) {
+    public static void printSecretLength(MySecret<String> mySecret) {
         //int secretLen = mySecret.getMySecret().length();
 
         int secretLen;
@@ -45,5 +59,38 @@ public class MySecret {
         }
  */
        System.out.println("Mt secret has " + secretLen + " characters" );
+    }
+ //zad  Optional + Generyki . Napisz  metode generyczna, tworzaca Optional z T  + wariacje
+    public static <T> void processSecret(T optional){
+
+        // make optional from T
+        Optional<T> opt = Optional.ofNullable(optional);
+
+        // make optional from number
+        int number = 4;
+        Optional<Integer> numberInsideOptional = Optional.of(number);
+
+    //wyciagnij zawartosc z optionala (pudelka)
+
+        T unpacked = opt.get(); //Do not do that!!! - metodka get() nie obsluguje nulla
+
+        // rozwiazanie za pomoca if-ow
+        if(opt.isPresent()){
+            unpacked = opt.get();
+        } else {
+            unpacked = null;
+        }
+
+        // rozwiazanie metodka orElse() ( to to samo co z if-ami ale krocej)
+        unpacked = opt.orElse(null);
+
+        // rozwiazanie z uzyciem genrykow i lambd (b. skomplikowane) oraz uzywajac rzutowania (T)
+        unpacked = (T) opt.or(()-> null);
+
+
+        //chcemy sprawdzic zawartosc (malego) stringa - zwwrocic optionala(od <String>a), a jesli jest on pusty to inny obiekt
+        Optional<String> string = Optional.empty();
+        //robimy to uzywajac referencje FromOptional
+        Optional<String> FromOptional = string.or(() -> Optional.of("no value"));
     }
 }
